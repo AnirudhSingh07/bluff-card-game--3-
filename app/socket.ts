9e07bbb478084a5c174c3aafb85a28f6f0bc06c7
Anirudh Singh Chouhan <140189477+AnirudhSingh07@users.noreply.github.com>
2026-01-15 18:48:18 +0000

    server

--- app/socket.ts
+++ app/socket.ts
@@ -5,7 +5,7 @@ let socket: Socket | null = null;
 
 export const getSocket = (): Socket => {
   if (!socket) {
-    socket = io("http://localhost:4000", {
+    socket = io("https://bluff-game-server-production.up.railway.app", {
       autoConnect: false, // we control when to connect
     });
   }
